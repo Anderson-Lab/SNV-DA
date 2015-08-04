e53983022ff1ce1984b14e0ad4304da70cc36d81
Matt Paul <mattpaul@levyvlan538.1361.apn.wlan.wireless-pennnet.upenn.edu>
2015-08-04 14:03:12 -0400

    cleaning up dir

--- predict_opls.m
+++
@@ -1,4 +0,0 @@
-function pred = predict_opls(xtrain,ytrain,xtest,num_OPLS_fact)
-[model,stats] = opls(xtrain,ytrain,num_OPLS_fact);
-[t, t_ortho, Y_pred] = apply_opls_model(xtrain, ytrain, model, xtest);
-pred = Y_pred;
--- spls2/run_jobs2.sh
+++
@@ -1,7 +0,0 @@
-#Rscript parallel_find_opt_sPLS.R SNVM.unsup_filtered.csv SNVM.unsup_loci.csv none all_parallel 10 1500 50 
-Rscript parallel_permutation_sPLS.R SNVM.unsup_filtered.csv SNVM.unsup_loci.csv nonsyn nonsyn_perm 488 1000 .801
-Rscript parallel_permutation_sPLS.R SNVM.unsup_filtered.csv SNVM.unsup_loci.csv intronic intronic_perm 18 1000 .805
-Rscript parallel_permutation_sPLS.R SNVM.unsup_filtered.csv SNVM.unsup_loci.csv UTR3 UTR3_perm 36 1000 .730
-Rscript parallel_permutation_sPLS.R SNVM.unsup_filtered.csv SNVM.unsup_loci.csv ncRNA ncRNA_perm 13 1000 .689
-
-
