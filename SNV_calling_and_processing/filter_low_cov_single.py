7525cdb1b954ae8ac1cb83f093b4d9cff04f1e79
Matt Paul <mattpaul@CBI-CHODO-033.local>
2015-11-14 18:41:49 -0500

    updated filter script

--- SNV_calling_and_processing/filter_low_cov_single.py
+++ SNV_calling_and_processing/filter_low_cov_single.py
@@ -5,14 +5,11 @@ cov = sys.argv[1]
 inName = sys.argv[2]
 outName = sys.argv[3]
 
-snvs = set()
-for x in open("main.snvs", "r"):
-	snvs.add(x.split("_")[1])
 
 input = open(inName , "r")
 out = open(outName, "w")
 for row in input:
 	split = row.split("\t") 
 	numReads = int(split[2].split(",")[0])
-	if numReads >= int(cov) and split[0]+":" + split[1] in snvs:
+	if numReads >= int(cov):
 		out.write(row)
